b2cf1946fda77d7a7a75464abaf05baedc5ce979
Mehmet Nizam Saltan <nizamsaltan@protonmail.com>
2022-10-25 19:16:13 +0300

    Added drawboard scale

--- lib/widgets/drawboard_widget.dart
+++ lib/widgets/drawboard_widget.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:note_bus/tools/freesketch.dart';
 import 'package:note_bus/main.dart';
@@ -7,6 +8,7 @@ import 'package:note_bus/models/enums.dart';
 import 'package:note_bus/tools/save_project.dart';
 import 'package:note_bus/widgets/control_widget.dart';
 import 'package:perfect_freehand/perfect_freehand.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 import '../models/hand_sketch.dart';
 
@@ -17,14 +19,16 @@ class Drawboard extends StatefulWidget {
   State<Drawboard> createState() => _DrawboardState();
 }
 
-double touchPressure = 5; // Change it later on
 List<HandSketch> drawboardSketches = [];
 Offset drawboardOffset = const Offset(0, 0);
+double drawboardScale = 1;
 
 final GlobalKey globalWidgetKey = GlobalKey();
 
 class _DrawboardState extends State<Drawboard> {
   late HandSketch currentShape;
+  bool isControllKeyPressing = false;
+  double touchPressure = 5; // Change it later on
 
   // ** Callbacks **
 
@@ -32,8 +36,10 @@ class _DrawboardState extends State<Drawboard> {
     switch (currentMode) {
       case EditMode.pen:
         List<Point> x = [
-          Point(details.globalPosition.dx - drawboardOffset.dx,
-              details.globalPosition.dy - drawboardOffset.dy, 2),
+          Point(
+              details.globalPosition.dx / drawboardScale - drawboardOffset.dx,
+              details.globalPosition.dy / drawboardScale - drawboardOffset.dy,
+              2),
         ];
         currentShape =
             HandSketch(x, currentTheme.currentPenColor, touchPressure);
@@ -54,8 +60,10 @@ class _DrawboardState extends State<Drawboard> {
       case EditMode.pen:
         setState(() {
           currentShape.points.add(
-            Point(details.globalPosition.dx - drawboardOffset.dx,
-                details.globalPosition.dy - drawboardOffset.dy, 2),
+            Point(
+                details.globalPosition.dx / drawboardScale - drawboardOffset.dx,
+                details.globalPosition.dy / drawboardScale - drawboardOffset.dy,
+                2),
           );
         });
         break;
@@ -125,6 +133,19 @@ class _DrawboardState extends State<Drawboard> {
   @override
   Widget build(BuildContext context) {
     return Listener(
+      onPointerSignal: (event) {
+        if (event is PointerScrollEvent && isControllKeyPressing) {
+          if (event.scrollDelta.dy > 0) {
+            setState(() {
+              drawboardScale += .1;
+            });
+          } else if (event.scrollDelta.dy < 0) {
+            setState(() {
+              drawboardScale += -.1;
+            });
+          }
+        }
+      },
       onPointerDown: (event) {
         if (event.buttons != 1) {}
       },
@@ -135,48 +156,60 @@ class _DrawboardState extends State<Drawboard> {
           });
         }
       },
-      child: GestureDetector(
-        onPanStart: onPanStart,
-        onPanUpdate: onPanUpdate,
-        onPanCancel: onPanCancel,
-        onPanEnd: onPanEnd,
-        child: RepaintBoundary(
-          key: globalWidgetKey,
-          child: Scaffold(
-              backgroundColor: currentTheme.backgroundColor,
-              body: Stack(
-                children: [
-                  Transform.translate(
-                    offset: drawboardOffset,
-                    child: CustomPaint(
-                        painter: StrokePainter(shapes: drawboardSketches)),
-                  ),
-                  const ControlWidget(),
-                  Row(
-                    children: [
-                      TextButton(
-                        onPressed: ProjectSaver.instance.capturePng,
-                        child: Text('Capture Image',
-                            style: TextStyle(color: Colors.grey[900])),
-                      ),
-                      TextButton(
-                        onPressed: ProjectSaver.instance.saveFile,
-                        child: Text(
-                          'Save file',
-                          style: TextStyle(color: Colors.grey[900]),
+      child: RawKeyboardListener(
+        autofocus: true,
+        focusNode: FocusNode(),
+        onKey: (value) {
+          setState(() {
+            isControllKeyPressing = value.isControlPressed;
+          });
+        },
+        child: GestureDetector(
+          onPanStart: onPanStart,
+          onPanUpdate: onPanUpdate,
+          onPanCancel: onPanCancel,
+          onPanEnd: onPanEnd,
+          child: RepaintBoundary(
+            key: globalWidgetKey,
+            child: Scaffold(
+                backgroundColor: currentTheme.backgroundColor,
+                body: Stack(
+                  children: [
+                    Transform(
+                      origin: -drawboardOffset,
+                      transform: Matrix4.translation(vector.Vector3(
+                          drawboardOffset.dx, drawboardOffset.dy, 0))
+                        ..scale(drawboardScale),
+                      child: CustomPaint(
+                          painter: StrokePainter(shapes: drawboardSketches)),
+                    ),
+                    const ControlWidget(),
+                    Row(
+                      children: [
+                        TextButton(
+                          onPressed: ProjectSaver.instance.capturePng,
+                          child: Text('Capture Image',
+                              style: TextStyle(color: Colors.grey[900])),
                         ),
-                      ),
-                      TextButton(
-                        onPressed: ProjectSaver.instance.loadFile,
-                        child: Text(
-                          'Load file',
-                          style: TextStyle(color: Colors.grey[900]),
+                        TextButton(
+                          onPressed: ProjectSaver.instance.saveFile,
+                          child: Text(
+                            'Save file',
+                            style: TextStyle(color: Colors.grey[900]),
+                          ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
-              )),
+                        TextButton(
+                          onPressed: ProjectSaver.instance.loadFile,
+                          child: Text(
+                            'Load file',
+                            style: TextStyle(color: Colors.grey[900]),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )),
+          ),
         ),
       ),
     );
